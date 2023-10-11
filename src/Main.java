c9fba5fe002d7458ee4b78193625661a97e649be
621352 <camarenburt>
2023-10-11 09:07:33 -0400

    rsgd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+    double item;
+    double tax;
+    double finalPrice;
+
+        System.out.println("What is the price of your item? ");
+        item = scan.nextDouble();
+
+        if(item>=100){
+                System.out.println("Your final price is "+ item +".");
+            }else{
+                tax = item * 0.02;
+                finalPrice = item + tax;
+                System.out.println("Your final price is "+ finalPrice +".");
+        }
     }
 }
